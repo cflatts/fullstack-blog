f8deb5e4e7d6174577cf387d6341c6386f4e84e8
Caitlin Flattery <caitlinflattery@gmail.com>
2016-07-18 17:23:31 -0500

    submit handlers for sign in and register

--- src/scripts/views/LoginView.js
+++ src/scripts/views/LoginView.js
@@ -13,16 +13,25 @@ const LoginView = React.createClass ({
 })
 
 const Login = React.createClass ({
+
+    _handleRegister: function() {
+
+    },
+
+    _handleLogin: function() {
+
+    },
+
     render: function() {
         return (
             <div className = 'loginForm'>
-                <form className = 'signIn' id = 'register'>
+                <form className = 'signIn' id = 'register' onSubmit = {this._handleRegister} >
                     <h3>Register</h3>
                     <input type = 'text' name = 'registerName' placeholder = 'username' />
                     <input type = 'text' name = 'registerPassword' placeholder = 'password' />
                     <button type = 'submit'>Submit</button>
                 </form>
-                <form className = 'signIn' id = 'login'>
+                <form className = 'signIn' id = 'login' onSubmit = {this._handleLogin} >
                     <h3>Login</h3>
                     <input type = 'text' name = 'loginName' placeholder = 'username' />
                     <input type = 'text' name = 'loginPassword' placeholder = 'password' />
