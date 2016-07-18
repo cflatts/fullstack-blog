965c18dc2784a5d36582d246898958b6fde1341d
Caitlin Flattery <caitlinflattery@gmail.com>
2016-07-18 08:59:04 -0500

    updated schema page

--- src/scripts/models/models.js
+++ src/scripts/models/models.js
@@ -1,61 +1,25 @@
-import Backbone from 'backbone'
-import $ from 'jquery'
-import {app_name} from '../app'
+const mongoose = require('mongoose');
+const createModel = mongoose.model.bind(mongoose);
+const Schema = mongoose.Schema;
 
+// ----------------------
+// USERS
+// ----------------------
+const usersSchema = new Schema({
+  // required for authentication: DO NOT TOUCH Or You May Get Punched
+  email:     { type: String, required: true },
+  password:  { type: String, required: true },
+  // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x
 
-// ..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x
-// ..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x
-
-const User = Backbone.Model.extend({
-	urlRoot: '/api/users',
-	idAttribute: '_id'
-})
-User.register = function(email,password) {
-	return $.ajax({
-		type: 'post',
-		url: '/auth/register',
-		data: {
-			email: email,
-			password: password
-		}
-	})
-}
-
-User.login = function(email,password) {
-	return $.ajax({
-		type: 'post',
-		url: '/auth/login',
-		data: {
-			email: email,
-			password: password
-		}
-	}).then((userData) => {
-		localStorage[app_name + '_user'] = JSON.stringify(userData)
-		return userData
-	})
-}
-
-User.logout = function() {
-	return $.getJSON('/auth/logout').then(()=>{
-		localStorage[app_name + '_user'] = null
-	})
-}
-
-User.getCurrentUser = function() {
-	return localStorage[app_name + '_user'] ? JSON.parse(localStorage[app_name + '_user']) : null
-}
-
-
-export { User }
-
-// ..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x
-// ..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x
-
-// ^^ DO NOT TOUCH ^^
-// but, you may ...
-const myUserModel = User.extend({
+   // example of optional fields
+  name:      { type: String },
+  createdAt: { type: Date, default: Date.now }
 
 })
+
+module.exports = {
+  User: createModel('User', usersSchema)
+}
 
 export const PostModel = Backbone.Model.extend ({
     url: '/api/posts',
