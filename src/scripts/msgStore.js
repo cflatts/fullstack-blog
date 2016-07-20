d2d698d2639b48dfe143e56c50d8aba01dff57be
Caitlin Flattery <caitlinflattery@gmail.com>
2016-07-20 17:05:59 -0500

    created store object and set up data properties

--- src/scripts/msgStore.js
+++ src/scripts/msgStore.js
@@ -1,3 +1,10 @@
 import Backbone from 'backbone'
 import _ from 'underscore'
 import {PostCollection} from './models/models'
+
+const STORE = _.extend(Backbone.Events, {
+
+    data: {
+        collection: new PostCollection
+    }
+})
